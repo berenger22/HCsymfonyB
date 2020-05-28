c5ede92448939912b83972fdd6bc3aa49467812c
berenger22 <berenger.bodin@gmail.com>
2020-05-28 16:51:47 +0200

    the last dance

--- src/Controller/PartFPController.php
+++ src/Controller/PartFPController.php
@@ -103,11 +103,6 @@ class PartFPController extends AbstractController
 
     public function profil(Request $request, UserInterface $user)
     {
-        // dump($user);
-        //        $em = $this->getDoctrine()->getManager();    
-        //        $listeQuestions = $em
-        //                ->getRepository(Question::Class)
-        //                ->findBy($idProfesseur);
         $listeQuestions = $user->getQuestions();
         return $this->render('part_fp/profil_prof.html.twig', [
             'listeQuestions' => $listeQuestions
